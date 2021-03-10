c206f8753c0ee34f679a56dec58732a8401481e7
yash gupta <yash.gupta755@gmail.com>
2021-03-10 17:18:45 +0530

    URL

--- detail1.py
+++ detail1.py
@@ -13,6 +13,20 @@ file = open('do1.txt','r')
 
 for f in file:
 
+    headers = {
+        'sec-ch-ua': "\"Chromium\";v=\"88\", \"Google Chrome\";v=\"88\", \";Not A Brand\";v=\"99\"",
+        'sec-ch-ua-mobile': "?0",
+        'upgrade-insecure-requests': "1",
+        'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36",
+        'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        'sec-fetch-site': "none",
+        'sec-fetch-mode': "navigate",
+        'sec-fetch-user': "?1",
+        'sec-fetch-dest': "document",
+        'cache-control': "no-cache",
+        'postman-token': "f29fd632-08eb-6f9e-2834-62ba170bcd52"
+    }
+
     f = f.replace('\n','')
     page_url = 'https://www.redbook.com.au' + f
 
@@ -20,7 +34,7 @@ for f in file:
 
     # driver.get(page_url)
 
-    response = requests.request("GET", page_url)
+    response = requests.request("GET", page_url, headers=headers)
 
     # html2 = driver.page_source
     html = BeautifulSoup(response.content,  'html.parser')
